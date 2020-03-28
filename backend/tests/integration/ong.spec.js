46617c5532d39e839d915f1ad3940e17157c8f3c
jrocha90 <jvlimarocha@gmail.com>
2020-03-28 13:47:27 -0300

    Testes

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG',()=>{
+    beforeEach(async()=>{
+        await connection.migrate.latest();
+        });
+    it('should be able to create a new ONG',async()=>{
+        const response = await request(app)
+            .post('/ongs')
+            .send({            
+                name:"conato@teste.com",
+                whatsapp:"123",
+                city:"Minas Gerais",
+                uf:"MG"
+            
+
+        });
+
+        console.log(response.body);
+
+
+    });
+});
